2715ded2b3e89e31ad421ba73522cd0e706583f3
Ehsan Rezaee <ehsanrezaee29@gmail.com>
2019-09-29 23:12:34 +0330

    Change db to postgre

--- profillo/settings.py
+++ profillo/settings.py
@@ -76,8 +76,13 @@ WSGI_APPLICATION = 'profillo.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'profilodb',
+        'USER' : 'postgres',
+        'PASSWORD' : '85432093',
+        'HOST' : 'localhost',
+        'PORT' : '5432',
+
     }
 }
 
